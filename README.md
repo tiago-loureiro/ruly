b419db84be9e8304c76aa494c484fdbe34b9dc16
tiago-loureiro <tiago.loureiro@gmail.com>
2012-08-17 11:57:08 +0200

    First prototype

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ ruly
 ====
 
 Do you often get lost while looking for your friend's flat? Ruly is what you are looking for!
+
+Starting the app
+================
+
+Install node (this version was tested on node-v(0.8.7)
---
+++ server.js
@@ -0,0 +1,68 @@
+var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+var utils = require('util');
+var querystring = require('querystring');
+
+var serverAddress = "localhost";
+var serverPort = 8080;
+
+
+/*
+ * Handler for server static files
+ *
+ */
+function handleStaticFile(req, res) {
+	var filePath = '.' + req.url;
+  	if (filePath == './') {
+    	filePath = './static/index.html';
+  	}
+	path.exists(filePath, function(fileExists) {
+    	if (fileExists) {
+    		fs.readFile(filePath, function(error, content) {
+    			if (error) {
+        			res.writeHead(500);
+          			res.end();
+        		} else {
+          			var contentType = 'text/html';
+          			if (filePath.substr(-3) == '.js')
+            			contentType = 'application/javascript';
+          			var encoding = 'utf-8';
+          			res.writeHead(200, {'Content-Type': contentType});
+          			res.end(content, encoding);
+       			}
+      		});
+    	} else {
+    		console.log("Requested page not found: " + filePath	);
+			res.writeHead(404);
+			res.end()
+    	}
+	})
+}
+
+function handleLocation(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+ 	res.write("yeah are getting there");
+ 	res.end();
+}
+
+
+var serverHTTP = http.createServer(function (req, res) {
+
+	req.parsedUrl = url.parse(req.url);
+  	req.parsedUrl.parsedQuery = querystring.parse(req.parsedUrl.query || '');
+
+  	console.log("Request for url: " + req.url);
+
+  	switch (req.parsedUrl.pathname) {
+		case '/getLocation':
+	  		handleLocation(req, res);
+	  		break;
+		default:
+      		handleStaticFile(req, res);
+  }
+}).listen(serverPort, serverAddress);
+
+
+console.log('Server running..' + serverPort);
