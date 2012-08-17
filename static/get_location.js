1ba1fc23314978ddfa3f49f9a92d1a3e743bc14d
cowgirl <gaisavoir.le@gmail.com>
2012-08-17 13:02:05 +0200

    adding modernzr.js to get geolocation. updated index and get_location.js to return lat and lng to the server

--- static/get_location.js
+++ static/get_location.js
@@ -1,12 +1,25 @@
 window.onload = function() {
 	//Display both the progress and the description form
 	
-  	function checkLocation() {
-		var ts = new Date().getTime();
-    	$.get("/getLocation", { NoCache: ts}, function(data) {
-    		$("#mp3_location").text(data);
+  	function get_the_coords(position) {
+
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+    	$.get("/getLocation", { lat:latitude, lng:longitude}, function(data) {
+    		$("#mp3_location").text(data);alert(data);
 		});
-		setTimeout(checkLocation, 10000);
-  	}
-	checkLocation();
+    }
+    
+    function get_location() {
+        if (Modernizr.geolocation) {
+            navigator.geolocation.getCurrentPosition(get_the_coords);
+        } else {
+            alert ("HTML5 Geolocation does not work here");
+        }
+
+        setTimeout(get_location, 10000);
+    }
+    
+    get_location();
 }
